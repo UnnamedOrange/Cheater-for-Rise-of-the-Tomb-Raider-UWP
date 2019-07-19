ed969eeba74f68f6709f4f71b949ce135924a8eb
Orange Lee <33177912+UnnamedOrange@users.noreply.github.com>
2019-07-19 22:49:23 +0800

    创建工程

---
+++ Rise of the Tomb Raider Cheater/main.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+
+int WINAPI wWinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPWSTR, _In_ int)
+{
+	
+	return 0;
+}
---
+++ Rise of the Tomb Raider Cheater/pch.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#include "../../TKernel/TKernel/TKernel/kits/TKernel.hpp"
